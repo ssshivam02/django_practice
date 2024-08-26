7aaac8b082d86914c0015e510edb380a957b7bf7
ssshivam02 <shivamsharma199802@gmail.com>
2024-08-26 12:10:20 +0530

    messageframework

---
+++ messagesframework71v2608/messagesframework71v2608/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for messagesframework71v2608 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messagesframework71v2608.settings')
+
+application = get_asgi_application()
